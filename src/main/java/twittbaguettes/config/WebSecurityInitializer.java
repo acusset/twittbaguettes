6d321ae5001f53a8bddabd3632bcad350f80e3f8
Antoine Cusset <a.cusset@gmail.com>
2016-07-20 02:30:39 +0200

    merged

--- src/main/java/twittbaguettes/config/WebSecurityInitializer.java
+++
@@ -1,7 +0,0 @@
-package twittbaguettes.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class WebSecurityInitializer  extends AbstractSecurityWebApplicationInitializer{
-
-}
